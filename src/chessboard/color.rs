c6c872918dedd7f6f0c9f929a291ac7b3e07ff92
Alexander Gerstberger <a.gerstberger@web.de>
2025-03-27 10:25:54 +0100

    Implement From trait for Color and PieceType enums to convert from fen char to enum

--- src/chessboard/color.rs
+++ src/chessboard/color.rs
@@ -2,3 +2,13 @@ pub enum Color {
     White,
     Black,
 }
+
+impl From<char> for Color {
+    fn from(value: char) -> Self {
+        match value {
+            'w' => Color::White,
+            'b' => Color::Black,
+            _ => panic!("Invalid color: {}", value)
+        }
+    }
+}
--- src/chessboard/piece_type.rs
+++ src/chessboard/piece_type.rs
@@ -6,3 +6,17 @@ pub enum PieceType {
     Queen,
     King
 }
+
+impl From<char> for PieceType {
+    fn from(value: char) -> Self {
+        match value {
+            'P' => PieceType::Pawn,
+            'N' => PieceType::Knight,
+            'B' => PieceType::Bishop,
+            'R' => PieceType::Rook,
+            'Q' => PieceType::Queen,
+            'K' => PieceType::King,
+            _ => panic!("Invalid piece type: {}", value)
+        }
+    }
+}
